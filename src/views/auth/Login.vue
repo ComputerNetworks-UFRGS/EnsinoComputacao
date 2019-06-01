53bab2fb5a175c16d8614f2d7a894fda94a91da4
tete <tiagomdepaula@gmail.com>
2019-06-01 17:47:50 -0300

    Refatoração: login page.

--- src/views/auth/Login.vue
+++ src/views/auth/Login.vue
@@ -1,91 +1,69 @@
 <template>
-  <section class="section section-shaped section-lg my-0">
-    <div class="shape shape-style-1 bg-gradient-default">
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-    </div>
-    <div class="container pt-lg-md">
-      <div class="row justify-content-center">
-        <div class="col-lg-5">
-          <base-alert type="danger" dismissible v-if="errorMessage" @close="errorMessage=false">
-            <span class="alert-inner--text">
-              <strong>Ops!</strong>
-              {{ errorMessage }}
-            </span>
-            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-              <span aria-hidden="true">&times;</span>
-            </button>
-          </base-alert>
+  <section class="login">
+    <div class="container">
+      <div class="columns is-centered">
+        <div class="column is-5">
+          <div class="field">
+            <label class="label">E-mail</label>
+            <div class="control">
+              <input
+                v-model="email"
+                alternative
+                class="input"
+                placeholder="E-mail"
+                addon-left-icon="ni ni-email-83"
+              >
+            </div>
+          </div>
 
-          <div
-            type="secondary"
-            shadow
-            header-classes="bg-white pb-5"
-            body-classes="px-lg-5 py-lg-5"
-            class="border-0"
+          <div class="field">
+            <div class="label">Senha</div>
+            <div class="control">
+              <input
+                v-model="password"
+                alternative
+                class="input"
+                type="password"
+                placeholder="Senha"
+                addon-left-icon="ni ni-lock-circle-open"
+              >
+            </div>
+          </div>
+
+          <b-message
+            title="Ops!"
+            type="is-danger"
+            v-if="errorMessage" @close="errorMessage=false"
+            aria-close-label="Close message"
           >
-            <!-- <template>
-                            <div class="text-muted text-center mb-3">
-                                <small>Sign in with</small>
-                            </div>
-                            <div class="btn-wrapper text-center">
-                                <button type="neutral">
-                                    <img slot="icon" src="img/icons/common/github.svg">
-                                    Github
-                                </button>
+            {{ errorMessage }}
+          </b-message>
 
-                                <button type="neutral">
-                                    <img slot="icon" src="img/icons/common/google.svg">
-                                    Google
-                                </button>
-                            </div>
-            </template>-->
-            <template>
-              <div class="text-center text-muted mb-4">
-                <small>Acesso com e-mail e senha</small>
+          <div class="field">
+            <div class="control">
+              <button type="primary" class="button is-primary" @click="login">Entrar</button>
+            </div>
+          </div>
+
+          <hr>
+
+          <div class="level">
+            <div class="level-left">
+              <div class="level-item">
+                <router-link to="/recuperar-senha" class="text-light">
+                  <small>Esqueceu a senha?</small>
+                </router-link>
               </div>
-              <form role="form">
-                <input
-                  v-model="email"
-                  alternative
-                  class="mb-3"
-                  placeholder="E-mail"
-                  addon-left-icon="ni ni-email-83"
-                >
-                <input
-                  v-model="password"
-                  alternative
-                  type="password"
-                  placeholder="Senha"
-                  addon-left-icon="ni ni-lock-circle-open"
-                >
-                <!-- <input type="checkbox" 
-                                    Lembrar de mim
-                </input type="checkbox" -->
-                <div class="text-center">
-                  <button type="primary" class="my-4" @click="login">Entrar</button>
-                </div>
-              </form>
-            </template>
-          </div>
-          <div class="row mt-3">
-            <div class="col-6">
-              <router-link to="/recuperar-senha" class="text-light">
-                <small>Esqueceu a senha?</small>
-              </router-link>
             </div>
-            <div class="col-6 text-right">
-              <router-link to="/cadastro" class="text-light">
-                <small>Criar conta</small>
-              </router-link>
+            <div class="level-right">
+              <div class="level-item">
+                <router-link to="/cadastro" class="text-light">
+                  <small>Primeiro acesso</small>
+                </router-link>
+              </div>
             </div>
           </div>
+          <br>
         </div>
       </div>
     </div>
@@ -107,16 +85,21 @@ export default {
       AuthService.login({
         email: this.email,
         password: this.password
-      }).then(res => {
-        console.log("res", res);
-        if (res.status == 200) {
-          localStorage.setItem("token", res.data.token);
-          this.$store.commit("set_user", res.data);
-          this.$router.push("/dash");
-        } else {
-          this.errorMessage = res.data.message;
-        }
-      });
+      })
+        .then(res => {
+          console.log("res", res);
+          if (res.status == 200) {
+            localStorage.setItem("token", res.data.token);
+            this.$store.commit("set_user", res.data);
+            this.$router.push("/dash");
+          } else {
+            console.log("????", res.data.message);
+            this.errorMessage = res.data.message;
+          }
+        })
+        .catch(err => {
+          this.errorMessage = err.response.data.message;
+        });
     }
   }
 };
