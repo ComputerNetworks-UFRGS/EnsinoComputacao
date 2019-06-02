cccd3c85ee94e46f3ac5c12eaedfb10067ee5199
tete <tiagomdepaula@gmail.com>
2019-06-02 15:32:14 -0300

    Layout contato.

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,44 +1,47 @@
 <template>
-    <div class="profile-page">
-        <section class="section-profile-cover section-shaped my-0">
-            <div class="shape shape-style-1 shape-primary alpha-4">
-                <span></span>
-                <span></span>
-                <span></span>
-                <span></span>
-                <span></span>
-                <span></span>
+  <section class="contact-page">
+    <div class="container">
+      <br>
+      <div class="columns is-centered">
+        <div class="column is-4">
+          <h4 class="title is-4 has-text-centered">Contato</h4>
+
+          <div class="field">
+            <label class="label">Seu nome</label>
+            <div class="control">
+              <input class="input" placeholder="Seu nome">
             </div>
-        </section>
-        <section class="section section-lg pt-lg-0 section-contact-us">
-            <div class="container">
-                <div class="row justify-content-center mt--300">
-                    <div class="col-lg-8">
-                        <div gradient="secondary" shadow body-classes="p-lg-5">
-                            <h4 class="mb-1">Contato</h4>
-                            <p class="mt-0">...</p>
-                            <input class="mt-5"
-                                        alternative
-                                        placeholder="Seu nome"
-                                        addon-left-icon="ni ni-user-run">
-                            
-                            <input alternative
-                                        placeholder="Seu e-mail"
-                                        addon-left-icon="ni ni-email-83">
-                            
-                            <input class="mb-4">
-                                    <textarea class="form-control form-control-alternative" name="name" rows="4"
-                                              cols="80" placeholder="Digite aqui..."></textarea>
-                            
-                            <button type="default" round block size="lg">
-                                Enviar mensagem
-                            </button>
-                        </div>
-                    </div>
-                </div>
+          </div>
+
+          <div class="field">
+            <label class="label">Seu e-mail</label>
+            <div class="control">
+              <input class="input" placeholder="Seu e-mail">
             </div>
-        </section>
+          </div>
+
+          <div class="field">
+            <label class="label">Mensagem</label>
+            <div class="control">
+              <textarea
+                class="textarea"
+                name="name"
+                rows="4"
+                cols="80"
+                placeholder="Digite aqui..."
+              ></textarea>
+            </div>
+          </div>
+
+          <div class="field">
+            <div class="control">
+              <button class="button is-primary">Enviar mensagem</button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
+  </section>
 </template>
 <script>
 export default {};
