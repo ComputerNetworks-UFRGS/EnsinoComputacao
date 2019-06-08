6939951662eb831bf36c0c11d7a1fae71842398d
tete <tiagomdepaula@gmail.com>
2019-06-08 15:49:09 -0300

    Alterações Pedro: grafo GoJs atualizado.

--- src/components/GojsTest.vue
+++ src/components/GojsTest.vue
@@ -1,68 +1,219 @@
 <template>
-  <div id="grafoTeste" style="height: 750px; width: 1000px; border: 1px solid black;"></div>
+  <div>
+    <div class="col-md-6">
+      <ul class="nav nav-pills nav-fill nav-justified">
+        <li class="nav-item active">
+          <div class="btn btn-primary btn-lg btn-block" @click="setYearsModeFalse()">Tudo</div>
+        </li>
+        <li class="nav-item active">
+          <div class="btn btn-primary btn-lg btn-block" @click="setYearsModeTrue()">Anos</div>
+        </li>
+      </ul>
+    </div>
+    <div
+      id="grafoTeste"
+      style="height: 800px; width: 1800px; border: 0px solid black; display: block; margin: 0 auto"
+    ></div>
+  </div>
 </template>
 
 <script id="sample">
-import go from "../../node_modules/gojs/release/go-debug.js";
+import go, { Transaction } from "../../node_modules/gojs/release/go-debug.js";
 import json from "@/assets/data.json";
 export default {
+  props: {},
   data() {
     return {
+      builder: null,
       graph: null,
       nodes: json.nodes,
-      edges: json.links
+      edges: json.links,
+      yearsMode: false
     };
   },
-  methods: {},
+  methods: {
+    buildGraph() {
+      var $ = this.builder;
+
+      var grafo = this.graph;
+
+      var nodeDataArray = [];
+      var linkDataArray = [];
+
+      var groups = [];
+
+      this.nodes.forEach(node => {
+        if (this.yearsMode) {
+          var tempNode = {
+            key: node.id,
+            text: node.content.text,
+            color: node.content.color,
+            group: node.year
+          };
+          nodeDataArray.push(tempNode);
+          if (groups.indexOf(tempNode.group) === -1) {
+            var tempGroup = {
+              key: tempNode.group,
+              isGroup: true
+            };
+            nodeDataArray.push(tempGroup);
+            groups.push(tempNode.group);
+          }
+        } else {
+          var tempNode = {
+            key: node.id,
+            text: node.content.text,
+            color: node.content.color
+          };
+          nodeDataArray.push(tempNode);
+        }
+      });
+
+      grafo.model.linkDataArray = [];
+
+      groups.sort();
+
+      this.edges.forEach(edge => {
+        var tempEdge = {
+          to: edge.destination,
+          from: edge.source
+        };
+        if (!this.yearsMode) {
+          linkDataArray.push(tempEdge);
+        } else if (
+          nodeDataArray.find(node => node.key === tempEdge.to).group ===
+          nodeDataArray.find(node => node.key === tempEdge.from).group
+        ) {
+          linkDataArray.push(tempEdge);
+        }
+      });
+
+      for (var i = 0; i < groups.length - 1; i++) {
+        var tempEdge = {
+          to: groups[i + 1],
+          from: groups[i]
+        };
+        linkDataArray.push(tempEdge);
+      }
+
+      grafo.model = new go.GraphLinksModel(nodeDataArray, linkDataArray);
+
+      grafo.groupTemplate = $(
+        go.Group,
+        "Auto",
+        {
+          selectable: false
+        },
+        $(
+          go.Panel,
+          "Auto",
+          $(go.Shape, "RoundedRectangle", { fill: "#C3B5FF" }),
+          $(go.Placeholder, { padding: 5 }),
+          $(go.TextBlock, "Ano", new go.Binding("text", "key"), {
+            alignment: new go.Spot(1, 1),
+            font: "Normal normal normal 20pt Kreon, serif",
+            margin: 5
+          })
+        ),
+        $(go.LayeredDigraphLayout, {
+          direction: 90,
+          layeringOption: go.LayeredDigraphLayout.LayerLongestPathSource,
+          aggressiveOption: go.LayeredDigraphLayout.AggressiveMore
+        })
+      );
+
+      if (this.yearsMode) {
+        grafo.layout = $(go.TreeLayout, {
+          angle: 90
+        });
+      } else {
+        grafo.layout = $(go.LayeredDigraphLayout, {
+          direction: 90,
+          layeringOption: go.LayeredDigraphLayout.LayerLongestPathSource,
+          aggressiveOption: go.LayeredDigraphLayout.AggressiveMore
+        });
+      }
+
+      grafo.nodeTemplate = $(
+        go.Node,
+        "Auto",
+        {
+          selectable: false,
+          deletable: false,
+          mouseEnter: this.mouseEnter,
+          mouseLeave: this.mouseLeave,
+          cursor: "pointer",
+          click: this.nodeClicked
+        },
+        $(
+          go.Shape,
+          "RoundedRectangle",
+          {
+            stroke: "#233B30",
+            strokeWidth: 2,
+            name: "SHAPE"
+          },
+          new go.Binding("fill", "color")
+        ),
+        $(
+          go.TextBlock,
+          "Habilidade",
+          {
+            margin: 15,
+            font: "Normal normal normal 15pt Kreon, serif",
+            name: "TEXT"
+          },
+          new go.Binding("text", "text")
+        )
+      );
+    },
+    mouseEnter(e, obj) {
+      var shape = obj.findObject("SHAPE");
+      shape.fill = "#185137";
+      shape.stroke = "#94FACC";
+      var text = obj.findObject("TEXT");
+      text.stroke = "white";
+    },
+    mouseLeave(e, obj) {
+      var shape = obj.findObject("SHAPE");
+      // Return the Shape's fill and stroke to the defaults
+      shape.fill = obj.data.color;
+      shape.stroke = "#233B30";
+      // Return the TextBlock's stroke to its default
+      var text = obj.findObject("TEXT");
+      text.stroke = "black";
+    },
+    setYearsModeFalse() {
+      this.yearsMode = false;
+      this.buildGraph();
+    },
+    setYearsModeTrue() {
+      this.yearsMode = true;
+      this.buildGraph();
+    },
+    nodeClicked(e, obj) {
+      var node = obj.part;
+      console.log(node.data.key);
+      this.$router.push({ name: "tasks" });
+    }
+  },
   mounted() {
-    this.graph = go.GraphObject.make;
-    var $ = this.graph;
+    this.builder = go.GraphObject.make;
+    var $ = this.builder;
 
-    var grafo = $(go.Diagram, "grafoTeste");
-
-    var nodeDataArray = [];
-    var linkDataArray = [];
-
-    this.nodes.forEach(node => {
-      var tempNode = {
-        key: node.id,
-        text: node.content.text,
-        color: node.content.color
-      };
-      nodeDataArray.push(tempNode);
+    var grafo = $(go.Diagram, "grafoTeste", {
+      hoverDelay: 15,
+      "undoManager.isEnabled": true,
+      allowHorizontalScroll: false,
+      allowVerticalScroll: false
     });
 
-    grafo.model.linkDataArray = [];
-
-    this.edges.forEach(edge => {
-      var tempEdge = {
-        to: edge.destination,
-        from: edge.source
-      };
-      linkDataArray.push(tempEdge);
-    });
-
-    console.log(nodeDataArray, linkDataArray);
-
-    grafo.model = new go.GraphLinksModel(nodeDataArray, linkDataArray);
-
-    grafo.layout = $(go.TreeLayout);
-    grafo.layout.angle = 90;
-    grafo.layout.nodeSpacing = 40;
-
-    grafo.nodeTemplate = $(
-      go.Node,
-      "Auto",
-      $(
-        go.Shape,
-        "RoundedRectangle",
-        { fill: "white" },
-        new go.Binding("fill", "color")
-      ),
-      $(go.TextBlock, "text", new go.Binding("text", "text"))
-    );
+    this.graph = grafo;
+    this.buildGraph();
   }
 };
 </script>
 
-<style></style>
+<style>
+@import url("https://fonts.googleapis.com/css?family=Kreon&display=swap");
+</style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,18 +1,23 @@
 <template>
   <div class="profile-page">
-    <section class="section-profile-cover section-shaped my-0">
-      <div class="shape shape-style-1 shape-primary alpha-4">
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-      </div>
-    </section>
+    <div class="position-relative">
+      <!-- shape Hero -->
+      <section class="section-shaped my-0">
+        <div class="shape shape-style-1 shape-default">
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+        <div class="container shape-container d-flex"></div>
+      </section>
+      <!-- 1st Hero Variation -->
+    </div>
+
     <section class="section section-skew">
-      <div class="container">
-        <GojsTest></GojsTest>
+      <div class="section section-lg pt-lg-0">
+        <GojsTest class="graphContainer" :yearsMode="yearsMode"></GojsTest>
       </div>
     </section>
   </div>
@@ -22,8 +27,17 @@ import GojsTest from "@/components/GojsTest.vue";
 export default {
   components: {
     GojsTest
+  },
+  data() {
+    return {
+      yearsMode: false
+    };
   }
 };
 </script>
 <style>
+.graphContainer {
+  display: block;
+  margin: 0 auto;
+}
 </style>
