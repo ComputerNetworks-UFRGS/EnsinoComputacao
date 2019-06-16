58e21fb08808cd15074326f6a4979a1819ce0746
tete <tiagomdepaula@gmail.com>
2019-06-16 18:58:14 -0300

    Tela de boas vindas no dash.

---
+++ src/views/dash/DashWelcome.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <br>
+    <div class="card has-text-centered">
+      <div class="card-content">
+        Bem vindo ao Compeduba. Comece criando suas atividades.
+        <br><br>
+        <router-link class="button is-primary" to="/dash/atividades">Suas atividades</router-link>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
