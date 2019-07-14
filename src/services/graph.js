3bb4660f6422b17fa3e18fdcb0f82c03cd0087b3
tete <tiagomdepaula@gmail.com>
2019-07-14 16:20:11 -0300

    Ajuste rotas grafos.

--- src/services/graph.js
+++ src/services/graph.js
@@ -2,28 +2,28 @@ import API from './API.js'
 
 export default {
     list() {
-        return API.get('temp-graphs/')
+        return API.get('graphs/')
     },
     detail(graph_id) {
-        return API.get('temp-graphs/' + graph_id)
+        return API.get('graphs/' + graph_id)
     },
     create(graph) {
-        return API.post('temp-graphs/', graph)
+        return API.post('graphs/', graph)
     },
     remove(graph_id) {
-        return API.delete('temp-graphs/' + graph_id)
+        return API.delete('graphs/' + graph_id)
     },
     addNode(graph_id, data) {
-        return API.post('temp-graphs/' + graph_id + '/node', data)
+        return API.post('graphs/' + graph_id + '/node', data)
     },
     removeNode(graph_id, node_id) {
-        return API.delete('temp-graphs/' + graph_id + '/node/' + node_id)
+        return API.delete('graphs/' + graph_id + '/node/' + node_id)
     },
     addEdge(graph_id, data) {
-        return API.post('temp-graphs/' + graph_id + '/edge', data)
+        return API.post('graphs/' + graph_id + '/edge', data)
     },
     removeEdge(graph_id, from_id, to_id) {
-        let url = 'temp-graphs/' + graph_id + '/edge/' + from_id + '/' + to_id
+        let url = 'graphs/' + graph_id + '/edge/' + from_id + '/' + to_id
         return API.delete(url)
     },
 }
