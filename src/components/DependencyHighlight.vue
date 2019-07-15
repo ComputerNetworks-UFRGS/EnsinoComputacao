454628fa5f0618b42e9dedcc5a5b4fdc57d7a65a
tete <tiagomdepaula@gmail.com>
2019-07-14 22:20:02 -0300

    Highlight de nodo selecionado.

--- src/components/DependencyHighlight.vue
+++ src/components/DependencyHighlight.vue
@@ -4,11 +4,16 @@
       <div
         v-for="node of step"
         :key="node.id + Math.random()"
-        @click="mouseenter(node)"
+        @click="selectNode(node)"
         class="node"
-        :class="{'node-highlight': nodes[node.id].highlight}"
+        :class="{
+            'node-highlight': nodes[node.id].highlight,
+            'node-dependent': nodes[node.id].isDependent,
+            'node-current': nodes[node.id].isSelected
+        }"
       >
-        <!-- {{ node.id }})  -->
+        <!-- {{ node.id }})
+        [{{ node.step }}] -->
         {{ node.title }}
       </div>
       <br />
@@ -18,6 +23,7 @@
 
 <script>
 import Graphs from "@/services/graph";
+import _ from "lodash";
 
 export default {
   name: "dependency-highlight",
@@ -42,28 +48,43 @@ export default {
           for (let i in steps) {
             for (let node of steps[i]) {
               node["highlight"] = false;
+              node["isDependent"] = false;
+              node["isSelected"] = false;
+              node["step"] = i;
               this.$set(this.nodes, node.id, node);
             }
           }
           this.steps = steps;
         });
     },
-    mouseenter(node) {
+    selectNode(node) {
       this.clearHighlight();
-      this.highlightNodeUp(node);
+      this.nodes[node.id].isSelected = true;
+      this.highlightNodeUp(node.dependencies);
+      this.highlightNodeDown(node);
     },
     clearHighlight() {
       for (let step of this.steps) {
         for (let node of step) {
+          this.nodes[node.id].isSelected = false;
           this.nodes[node.id].highlight = false;
+          this.nodes[node.id].isDependent = false;
         }
       }
     },
-    highlightNodeUp(node) {
-      this.nodes[node.id].highlight = true;
-      if (node.dependencies) {
-        for (let dependency of node.dependencies) {
-          this.highlightNodeUp(this.nodes[dependency]);
+    highlightNodeUp(dependencies) {
+      for (let dependency of dependencies) {
+        this.nodes[dependency].highlight = true;
+        this.highlightNodeUp(this.nodes[dependency].dependencies);
+      }
+    },
+    highlightNodeDown(node) {
+      if (node.dependents) {
+        for (let dependent of node.dependents) {
+          let dep_node = this.nodes[dependent];
+          if (dep_node && dep_node.step == parseInt(node.step) + 1) {
+            dep_node.isDependent = true;
+          }
         }
       }
     }
@@ -73,10 +94,9 @@ export default {
 
 <style scoped>
 .steps {
-    display: flex;
-    max-width: 100%;
-    overflow-x: auto;
-
+  display: flex;
+  max-width: 100%;
+  overflow-x: auto;
 }
 .node {
   border: 1px solid #ddd;
@@ -85,5 +105,11 @@ export default {
 }
 .node-highlight {
   background: #f8f300;
+}
+.node-dependent {
+  background: #bef5ff;
+}
+.node-current {
+  background: #6ee974;
 }
 </style>
