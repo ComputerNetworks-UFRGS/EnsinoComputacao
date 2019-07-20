fffd49cbab6c2b98d77b4fd15819b582be4098ec
tete <tiagomdepaula@gmail.com>
2019-07-20 13:58:50 -0300

    Menu mobile dash.

--- src/views/dash/DashBase.vue
+++ src/views/dash/DashBase.vue
@@ -1,42 +1,61 @@
 <template>
   <div class="container pd">
+    <button @click="isMenuOpen = !isMenuOpen" class="is-hidden-tablet button button is-light" :class="{'is-menu-open': isMenuOpen}">
+      <span class="icon">
+        <i
+          class="fas"
+          :class="'fa-bars'"
+        ></i>
+      </span>
+      <span>Menu</span>
+      <span class="icon">
+        <i
+          class="fas"
+          :class="{
+            'fa-angle-down': isMenuOpen,
+            'fa-angle-right': !isMenuOpen,
+          }"
+        ></i>
+      </span>
+    </button>
+
     <div class="columns">
-      <div class="column is-4">
+      <div class="column is-4" :class="{'is-hidden-mobile': !isMenuOpen}">
         <aside class="menu">
           <p class="menu-label" v-auth="'task.list'">Criação</p>
           <ul class="menu-list">
             <li v-auth="'task.list'">
-              <router-link to="/dash/atividades">Suas atividades</router-link>
+              <a href="#!" @click.prevent="goTo('/dash/atividades')">Suas atividades</a>
             </li>
           </ul>
 
           <p class="menu-label" v-auth="'review.list'">Gerência</p>
           <ul class="menu-list">
             <li v-auth="'review.list'">
-              <router-link to="/dash/revisao">Revisão de atividades</router-link>
+              <a href="#!" @click.prevent="goTo('/dash/revisao')">Revisão de atividades</a>
             </li>
           </ul>
 
           <p class="menu-label" v-auth="'role.list|users.list'">Administração</p>
           <ul class="menu-list">
             <li v-auth="'role.list'">
-              <router-link to="/dash/papeis">Perfis de acesso</router-link>
+              <a href="#!" @click.prevent="goTo('/dash/papeis')">Perfis de acesso</a>
             </li>
             <li v-auth="'users.list'">
-              <router-link to="/dash/usuarios">Usuários</router-link>
+              <a href="#!" @click.prevent="goTo('/dash/usuarios')">Usuários</a>
             </li>
           </ul>
 
           <p class="menu-label" v-auth="'topics.list|skill.list|curri.list'">Organização</p>
           <ul class="menu-list">
             <li v-auth="'topics.list'">
-              <router-link to="/dash/objetos">Objetos de ensino</router-link>
+              <a href="#!" @click.prevent="goTo('/dash/objetos')">Objetos de ensino</a>
             </li>
             <li v-auth="'skill.list'">
-              <router-link to="/dash/habilidades">Habilidades</router-link>
+              <a href="#!" @click.prevent="goTo('/dash/habilidades')">Habilidades</a>
             </li>
             <li v-auth="'curri.list'">
-              <router-link to="/dash/curriculos">Currículos</router-link>
+              <a href="#!" @click.prevent="goTo('/dash/curriculos')">Currículos</a>
             </li>
           </ul>
         </aside>
@@ -51,7 +70,33 @@
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      isMenuOpen: false
+    };
+  },
+  methods: {
+    goTo(path) {
+      this.$router.push(path)
+      this.isMenuOpen = false
+    }
+  }
+};
 </script>
 
-<style></style>
+<style scoped>
+@media (max-width: 768px) {
+  .menu {
+    padding: 6px;
+    background: #f5f5f5;
+    border-bottom-right-radius: 12px;
+    border-bottom-left-radius: 12px;
+    border-top-right-radius: 12px;
+  }
+  .is-menu-open {
+    border-bottom-right-radius: 0px;
+    border-bottom-left-radius: 0px;
+  }
+}
+</style>
